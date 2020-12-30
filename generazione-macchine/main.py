e21a53b6229fb57fa832de7c5546085f11850203
napsav <savi.napolitano@gmail.com>
2020-12-30 16:54:44 +0100

    primo

---
+++ generazione-macchine/main.py
@@ -0,0 +1,28 @@
+import os
+import json
+path = "/disco3/SteamLibrary/steamapps/common/assettocorsa/content/cars/"
+listepath = [f.path for f in os.scandir(path) if f.is_dir()]
+final = []
+salvataggio = open("accars.json", "w+", encoding="utf-8")
+listamacchine = open("macchine.txt", "w+", encoding="utf-8")
+for macchina in listepath:
+    nome = macchina.replace("/disco3/SteamLibrary/steamapps/common/assettocorsa/content/cars/","")
+    print(macchina)
+    sub = [f.path for f in os.scandir(macchina) if f.is_dir()]
+    print(sub)
+    if (macchina + "/skins") in sub:
+        listaskins_path = [f.path for f in os.scandir(macchina + "/skins/") if f.is_dir()]
+        listaskins = [elem.replace(macchina + "/skins/","") for elem in listaskins_path]
+        x = {
+            nome:listaskins
+        }
+        final.append(x)
+    else:
+        x = {
+            nome: None
+        }
+        final.append(x)
+listamacchine.write("\n".join(["<option>" + elem.replace("/disco3/SteamLibrary/steamapps/common/assettocorsa/content/cars/","").strip("\n") + "</option>\n" for elem in listepath]))
+
+salvataggio.write(json.dumps(final, indent=2))
+
