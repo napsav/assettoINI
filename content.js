43d8610616e92b92d39d822608f1d71783b0c1e9
napsav <savi.napolitano@gmail.com>
2021-04-12 01:46:25 +0200

    content

--- content.js
+++ content.js
@@ -1,18 +1,34 @@
-const { readdirSync } = require('fs')
+const { readdirSync, existsSync } = require('fs')
 require('dotenv').config()
+const baseDir = process.env.ACPATH
 
 const getDirectories = source =>
   readdirSync(source, { withFileTypes: true })
     .filter(dirent => dirent.isDirectory())
     .map(dirent => dirent.name)
 
-
 function macchineDisponibili() {
-    return getDirectories(process.env.ACPATH+"cars/")
+    let final = {}
+    let listamain = getDirectories(baseDir+"cars/")
+    listamain.forEach(elem => {
+        let path = baseDir+"cars/"+elem+"/skins/";
+        if(existsSync(path)) {
+            final[elem] = getDirectories(path)
+        }
+    })
+    return final;
 }
 
 function mappeDisponibili() {
-    return getDirectories(process.env.ACPATH+"tracks/")
+/*     let final = {}
+    let listamain = getDirectories(baseDir+"tracks/")
+    listamain.forEach(elem => {
+        let path = baseDir+"tracks/"+elem+"/skins/";
+        if(existsSync(path)) {
+            final[elem] = getDirectories(path)
+        }
+    })
+    return final; */
 }
 
 module.exports = {
